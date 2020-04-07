74ae9640cfcb3a1e9b19e355072d01ddb12392fb
Andrew <Jendrula420@wp.pl>
2020-04-07 23:33:05 +0200

    v3

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.smartherd.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Przycisk zostal wcisniety")
+            Toast.makeText(this, "Buttons", Toast.LENGTH_SHORT).show()
+
+        }
+
+        btnStartSecondActivity.setOnClickListener {
+            val message = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val myIntent = Intent(this, SecondActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 
 }
